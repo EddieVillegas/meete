da4f99e13e324b01ba71e851c98cba34014fcbe4
Eddie <CustomGeek@hotmail.com>
2026-03-05 13:12:22 -0600

    Add metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import { Metadata } from "next"
+
 import Link from "next/link"
 import Sidebar from "@/components/Sidebar"
+
+export const metadata: Metadata = {
+  title: "Meeti - Home"
+}
 
 export default function Home(){
   return (
     <>
     <h1>Inicio</h1>
-    <Link href="/about">Ir a nosotros</Link>
+    <Link href="/about">Go to about</Link>
     <Sidebar/>
     </>
   )
